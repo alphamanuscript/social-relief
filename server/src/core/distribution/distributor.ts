285f52d202def46fe6c62698f761ec81b79bf7e6
Clément Habinshuti <hello@habbes.xyz>
2020-05-09 02:20:21 +0300

    Minor clean up

--- server/src/core/distribution/distributor.ts
+++ server/src/core/distribution/distributor.ts
@@ -47,33 +47,6 @@ interface CreateDistributionPlanResult {
   beneficiariesSummaries:DistributionPlanBeneficiariesSummaries;
 }
 
-/*
-PSEUDO CODE
-
-async function distributeDonations() {
-  create some global lock on the app that will prevent the app from running any
-  operation that could conflict with the process, e.g. operations like refunds that may deduct
-  the funds from users account, which would mess up the calculation of the balance
-
-  find Beneficiaries Who Have Not Reached Their Term Limit And Calculate Their Remainder();
-  get the donors for each beneficiary and calculate their balances
-  create map beneficiary -> remainingFunds
-  create map donor -> remainingBalance
-
-  init empty list/distribution plan of planned transactions { donor, beneficiary, amount }
-
-  for each beneficiary:
-    for each donor:
-      deduct upto beneficiary remaining (update donor and beneficiary map)
-      push result in distribution plan
-  
-  push list to transaction queue
-  transaction queue pulls items from the queue and executes the transactions
-  results should be logged, including start time and stop time
-}
-
-*/
-
 export class Distributor implements DonationDistributor {
   private db: Db;
   private periodLimit: number;
