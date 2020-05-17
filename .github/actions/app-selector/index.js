2a7c88d42909ed966a8a12ff835c18d4fffd028e
Clément Habinshuti <hello@habbes.xyz>
2020-05-18 00:06:51 +0300

    Add CI/CD pipeline
    Create custom action to encapsulate deployment
    
    Package deploy action into dist folder
    
    Fix heroku build
    
    Update action package
    
    Fix client deployment
    
    Package action
    
    Restore working review1 pipeline
    
    Fix client build step
    
    Make deployment work for review1, master and release
    
    Update action package
    
    Fix build
    
    Fix deploy
    
    Fix branch names in app selector
    
    Update pipeline
    
    Fix pipeline
    
    Fix bug in app-selector
    
    Fix heroku deployment

---
+++ .github/actions/app-selector/index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function selectAppTargets () {
+  const branch = github.context.ref;
+  switch (branch) {
+    case 'refs/heads/review1':
+      return setOutputs('review1');
+    case 'refs/heads/master':
+      return setOutputs('staging');
+    case 'refs/heads/release':
+      return setOutputs('production');
+    default:
+      core.setFailed(`Invalid target deployment branch '${branch}'`);
+  }
+}
+
+function setOutputs(appTarget) {
+  const herokuApp = core.getInput(`heroku-${appTarget}`);
+  const firebaseApp = core.getInput(`firebase-${appTarget}`);
+
+  if (!herokuApp) {
+    return core.setFailed(`Heroku app not specified for ${appTarget}`);
+  }
+
+  if (!firebaseApp) {
+    return core.setFailed(`FFirebase app not specified for ${appTarget}`);
+  }
+
+  core.setOutput('heroku-app', herokuApp);
+  core.setOutput('firebase-app', firebaseApp);
+}
+
+selectAppTargets();
