662f6c7ddfe9a0dbc70352d61d0df23b4222a2e6
Jason Mahirwe <bjthe2nd@gmail.com>
2020-05-18 19:44:25 +0200

    Install and set up helmet

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -2,9 +2,11 @@ import * as express from 'express';
 import * as cors from 'cors';
 import { App } from '../core';
 import { injectApp } from './middleware';
+import * as helmet from 'helmet';
 
 export function createServer(app: App) {
   const server = express();
+  server.use(helmet());
   server.use(express.json());
   server.use(cors());
   server.use(injectApp(app));
