f0b2ba186afa30d093e75da259b91a863ba92f38
Jason Mahirwe <bjthe2nd@gmail.com>
2020-05-18 22:50:57 +0200

    Limit each IP to 20 requests per minute

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -3,10 +3,12 @@ import * as cors from 'cors';
 import { App } from '../core';
 import { injectApp } from './middleware';
 import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 export function createServer(app: App) {
   const server = express();
   server.use(helmet());
+  server.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 20 })); // Limit each IP to 20 requests per minute
   server.use(express.json());
   server.use(cors());
   server.use(injectApp(app));
