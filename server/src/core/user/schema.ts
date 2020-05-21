3ce16c157b75d3829a45deb5917c708c4dff27dc
Jason Mahirwe <bjthe2nd@gmail.com>
2020-05-21 21:47:55 +0200

    Update regex pattern for validating phone numbers

--- server/src/core/user/schema.ts
+++ server/src/core/user/schema.ts
@@ -3,7 +3,7 @@ import * as joi from '@hapi/joi';
 const userCreateAndLoginSchema = joi.object().keys({
   phone: joi.string()
     .required()
-    .pattern(new RegExp(/^254\d{9}/))
+    .pattern(new RegExp(/^2547\d{8}$/)) // Starts with 2547 and ends with 8 digits
     .messages({
       'any.required': 'Phone is required',
       'string.base': 'Invalid type, phone must be a string',
@@ -21,53 +21,14 @@ const userCreateAndLoginSchema = joi.object().keys({
     })
 }); 
 
-export const userCreateSchema = joi.object().keys({
-  phone: joi.string()
-    .required()
-    .pattern(new RegExp(/^254\d{9}/))
-    .messages({
-      'any.required': 'Phone is required',
-      'string.base': 'Invalid type, phone must be a string',
-      'string.empty': 'Please enter your phone number',
-      'string.pattern.base': 'Invalid phone number. Must start with 2547 and be 12 digit long'
-    }),
-  password: joi.string()
-    .required()
-    .pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~!@#$%^&*])(?=.{8,18}$)[a-zA-Z][a-zA-Z\d]*[~!@#$%^&*?<>]*$/))
-    .messages({
-      'any.required': 'Password is required',
-      'string.base': 'Invalid type, password must be a string',
-      'string.empty': 'Please enter your password',
-      'string.pattern.base': 'Invalid password. Must range between 8 and 18 characters and have at least one uppercase, lowercase, digit, and special character'
-    })
-}); 
+export const userCreateSchema = userCreateAndLoginSchema; 
 
-export const userLoginSchema = joi.object().keys({
-  phone: joi.string()
-    .required()
-    .pattern(new RegExp(/^254\d{9}/))
-    .messages({
-      'any.required': 'Phone is required',
-      'string.base': 'Invalid type, phone must be a string',
-      'string.empty': 'Please enter your phone number',
-      'string.pattern.base': 'Invalid phone number. Must start with 2547 and be 12 digit long'
-    }),
-  password: joi.string()
-    .required()
-    .pattern(new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~!@#$%^&*])(?=.{8,18}$)[a-zA-Z][a-zA-Z\d]*[~!@#$%^&*?<>]*$/))
-    .messages({
-      'any.required': 'Password is required',
-      'string.base': 'Invalid type, password must be a string',
-      'string.empty': 'Please enter your password',
-      'string.pattern.base': 'Invalid password. Must range between 8 and 18 characters and have at least one uppercase, lowercase, digit, and special character'
-    })
-}); 
-
+export const userLoginSchema = userCreateAndLoginSchema;
 
 export const userNominateBeneficiary = joi.object().keys({
   phone: joi.string()
     .required()
-    .pattern(new RegExp(/^254\d{9}/))
+    .pattern(new RegExp(/^2547\d{8}$/)) // Starts with 2547 and ends with 8 digits
     .messages({
       'any.required': 'Phone is required',
       'string.base': 'Invalid type, phone must be a string',
@@ -76,7 +37,7 @@ export const userNominateBeneficiary = joi.object().keys({
     }),
   nominator: joi.string()
     .required()
-    .pattern(new RegExp(/^254\d{9}/))
+    .pattern(new RegExp(/^2547\d{8}$/))
     .messages({
       'any.required': `Nominator is required`,
       'string.base': 'Invalid type, nominator must be a string',
