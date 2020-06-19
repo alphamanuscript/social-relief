449a22ae7cdcd31bc2a32dee7ee0fe5ea167c8a1
Jason Mahirwe <bjthe2nd@gmail.com>
2020-06-19 20:29:31 +0200

    Implement the beneficiairies page

--- webapp/src/app.vue
+++ webapp/src/app.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="app">
     <div v-if="showLoggedInNavigation" class="logged-in-structure">
-      <div class="sidebar"></div>
+      <div class="sidebar">
+        <div class="logo-container">
+          <img :src="imageUrl" alt="Social Relief Logo" class="logo">
+        </div>
+        <div class="current-balance-container">
+          <span>Current balance</span>
+          <span>KSH</span>
+          <span>1,500</span>
+        </div>
+        <ul class="vertical-navigation">
+          <li class="active">Beneficiaries</li>
+          <li>Middlemen</li>
+          <li>My invitations</li>
+          <li>History</li>
+        </ul>
+        <div class="policy-and-terms">
+          <span>privacy policy</span>
+          <span>terms of use</span>
+          <span>(c)2020</span>
+        </div>
+      </div>
       <router-view class="view-content" />
     </div>
     <div v-else class="logged-out-structure">
@@ -43,7 +63,7 @@ export default {
     ...mapState(['user', 'message', 'transactions']),
     showLoggedInNavigation () {
       this.showPageOrRedirect();
-      if (this.$route.name === 'home') return false
+      if (this.$route.name === 'sign-in') return false
       return true
     },
     imageUrl () {
@@ -146,19 +166,124 @@ export default {
   }
 
   .logged-in-structure {
-    border: 1px solid #000;
+    // border: 1px solid #000;
     display: grid;
     grid-template-rows: 100vh;
-    grid-template-columns: 12rem 1fr;
+    grid-template-columns: 13rem 1fr;
 
     .sidebar {
-      border: 1px solid green;
+      // border: 1px solid green;
+      background-color: #FFF;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: space-between;
+      padding: 0;
+      box-shadow: 2px 0 5px #E2E2E2;
+      z-index: 999;
+
+      .logo-container {
+        // border: 1px solid #000;
+        width: 30%;
+        height: 3rem;
+        margin: 1rem 0 0 3rem;
+
+
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+
+      .current-balance-container {
+        // border: 1px solid #000;
+        background-color: #9D1A63;
+        height: 5rem;
+        width: 65%;
+        margin: -4rem 0 0 2rem;
+        border-radius: 1rem; 
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        box-shadow: 0 2px 5px #E2E2E2;
+
+        span {
+          color:  #FFF;
+          font-size: .7rem;
+          // border: 1px solid #000;
+          display: block;
+          height: 1rem;
+          width: 6rem;
+
+          &:not(:last-child) {
+            margin-bottom: .15rem;
+            font-weight: 400;
+          }
+
+          &:last-child {
+            font-size: .9rem;
+            font-weight: 500;
+          }
+        }
+      }
+
+      .vertical-navigation {
+        // border: 1px solid #000;
+        width: 57%;
+        margin: -5rem 0 2rem 2rem;
+        padding: 0;
+
+        li {
+          // border: 1px solid #000;
+          list-style: none;
+          padding: 0 0 0 1rem;
+          margin-bottom: 1.2rem;
+          font-size: .9rem;
+
+          &:not(:first-child) {
+            color: lighten(#000, 20%);
+            font-weight: 500;
+          }
+
+          &:hover {
+            cursor: pointer;
+          }
+
+        }
+
+        .active {
+          border-radius: 1rem;
+          background-color: #9D1A63;
+          color: #FFF;
+          font-weight: bold;
+          height: 1.5rem;
+          // border: none;
+          // font-size: .9rem;
+        }
+      }
+
+      .policy-and-terms {
+        // border: 1px solid #000;
+        height: 5rem;
+        width: 70%;
+        color: #9D1A63;
+        font-size: .7rem;
+        margin: 0 0 0 2rem;
+
+        span{
+          // border: 1px solid #000;
+          display: block;
+          margin-bottom: .3rem;
+        }
+      }
     }
 
     .view-content {
-      border: 1px solid red;
       height: 100vh;
       overflow-y: scroll;
+      background: #F5F5F5;
+      padding: 0 3rem 0 3rem;
     }
   }
 }
--- webapp/src/views/beneficiaries.vue
+++ webapp/src/views/beneficiaries.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="outermost-container">
-    This is the beneficiaries page
+    <div class="inner-container">
+      <div class="top-nav">
+        <div class="links">
+          <span class="nominate-btn">Nominate</span>
+          <span class="donate-btn">Donate</span>
+        </div>
+        <div class="account-details-and-settings">
+          <div>
+            <span class="user-name">John Doe</span>
+            <span class="account-number">
+              <span>Account Number:</span><span> 1234567</span>
+            </span>
+          </div>
+          <i class="fas fa-user-circle"></i>
+        </div>
+      </div>
+      <div class="page-intro">
+        <h3>Beneficiaries</h3>
+        <p>
+          <span>All nominees get <span class="max-amount-per-month">Ksh 3,500</span> to purchase basic supplies during this trying period. Your contribution will go a long way touch the lives of <span class="beneficiary-count">13,600 +</span> people who </span>
+          <span>are currently enlisted as beneficiaries of this system. We at social relief want to say a big <span class="thank-you-span">THANK YOU</span> for your kindness and support.</span>
+        </p>
+      </div>
+      <div class="my-nominees">
+        <h3>My Nominees</h3>
+        <div class="table-container">
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -16,6 +45,165 @@ export default {
 </script>
 <style lang="scss" scoped>
   .outermost-container {
-    border: 1px solid #000;
+    // border: 1px solid #000;
+    .inner-container {
+      // border: 1px solid #000;
+
+      .top-nav {
+        // border: 1px solid #000;
+        margin-top: 1.2rem;
+        height: 2.9rem;
+        display: flex;
+        justify-content: space-between;
+
+        .links {
+          // border: 1px solid #000;
+          width: 17rem;
+          height: 100%;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+
+          span {
+            // border: 1px solid #000;
+            font-weight: bold;
+
+            &:hover {
+              cursor: pointer;
+            }
+          }
+
+          .nominate-btn {
+            height: 2rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .donate-btn {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 3rem;
+            width: 8rem;
+            height: 2rem;
+            text-align: center;
+            background: #EF5A24;
+            color: #fff;
+            transition: all 0.5s;
+            box-shadow: 0 2px 5px #E2E2E2;
+            border: none;
+
+            &:hover {
+              background-image: linear-gradient(to bottom, #EF5A24, #9D1A63);
+            }
+          }
+        }
+
+        .account-details-and-settings {
+          // border: 1px solid #000;
+          height: 100%;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+
+          div {
+            // border: 1px solid #000;
+            display: flex;
+            flex-direction: column;
+            font-size: .8rem;
+            height: 100%;
+
+            span {
+              // border: 1px solid #000;
+              height: 100%;
+              display: flex;
+              justify-content: flex-end;
+            }
+
+            .user-name {
+              font-size: .8rem;
+              align-items: flex-end;
+            }
+
+            .account-number {
+              align-items: flex-start;
+              span {
+                &:first-child {
+                  color: #EF5A24;
+                  font-weight: bold;
+                }
+
+                &:last-child {
+                  color: #9D1A63;
+                }
+              }
+            }
+          }
+
+          i {
+            // border: 1px solid #000;
+            height: 100%;
+            width: 3rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 2.2rem;
+            color: #9D1A63;
+          }
+        }
+      }
+
+      .page-intro {
+        // border: 1px solid #000;
+        margin-top: 1.5rem;
+
+        h3 {
+          color: #EF5A24;
+        }
+
+        p {
+          margin-top: 1rem;
+          // border: 1px solid #000;
+          font-size: .8rem;
+
+          span {
+            // border: 1px solid #000;
+            display: block;
+            margin-bottom: .1rem;
+
+            &:first-child {
+              span {
+                display: inline-block;
+                color: #9D1A63;
+              }
+            }
+
+            &:last-child {
+              span {
+                display: inline-block;
+                color: #9D1A63;
+              }
+            }
+          }
+        }
+      }
+
+      .my-nominees {
+        margin-top: 1.5rem;
+        // border: 1px solid #000;
+        h3 {
+          color: #EF5A24;
+        }
+
+        .table-container {
+          // border: 1px solid #000; 
+          background-color: #FFF;
+          height: 22rem;
+          border-radius: .8rem;
+          box-shadow: 0 2px 5px #E2E2E2;
+        }
+      }
+    }
   }
 </style>
