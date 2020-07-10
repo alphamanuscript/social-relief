367af5e30ed9c885b56baa14738e79c373f5f6b0
Lydia Nishimwe <nishimwelydia@yahoo.com>
2020-07-10 05:21:10 +0200

    default beneficiary page ; progress calculation

--- webapp/src/views/beneficiaries.vue
+++ webapp/src/views/beneficiaries.vue
@@ -1,21 +1,26 @@
 <template>
   <b-container class="mw-100 my-3 px-lg-2 px-xl-5">
     <div class="ml-lg-5">
-      <div class="small">
+      <div class="">
         <h3 class="text-primary">Beneficiaries</h3>
         <p>
-          All nominees get <span class="text-secondary font-weight-bold">Ksh 2,000</span> to purchase basic supplies during this trying period. Your contribution will go a long way touch the lives of <span class="text-secondary font-weight-bold">13,600+</span> people who
+          All nominees get up to <span class="text-secondary font-weight-bold">Ksh 2,000</span> monthly to purchase basic supplies during this trying period. Your contribution will go a long way touch the lives of <span class="text-secondary font-weight-bold">13,600+</span> people who
           are currently enlisted as beneficiaries of this system. We at Social Relief want to say a big <span class="text-secondary font-weight-bold">THANK YOU</span> for your kindness and support.
         </p>
       </div>
       <div class="">
         <h3 class="text-primary pb-3">My Nominees</h3>
-        <b-table :items="beneficiaries" :fields="beneficiaryFields" striped hover stacked="sm" class="bg-white rounded shadow">
+        <div v-if="!beneficiaries.length" class="text-center">
+          <p class="h2 font-weight-light">You don't have any beneficiaries yet...</p>
+          <p class="">You can add a beneficiary directly, or nominate a middleman to add beneficiaries on your behalf.</p>
+          <p> <b-link to="nominate" exact exact-active-class="active" class="text-primary">Click here to get started.</b-link> </p>
+        </div>
+        <b-table v-else :items="beneficiaries" :fields="beneficiaryFields" striped hover stacked="sm" class="bg-white rounded shadow">
           <template v-slot:cell(index)="data">
             <span class="font-weight-bold">{{ data.index + 1 }}.</span>
           </template>
           <template v-slot:cell(progress)="data">
-            <span class="text-secondary font-weight-bold"> {{ data.item.progress }} %</span>
+            <span class="text-secondary font-weight-bold"> {{ getProgress(data.item._id) }} %</span>
           </template>
           <template v-slot:cell(expand)="data">
             <b-button variant="outline-primary" @click="handleExpand(data.item)" class="border-0"><i class="fas fa-expand"></i></b-button>
@@ -51,7 +56,7 @@
       <h5 class="text-secondary">
         Transaction history
       </h5>
-      <b-table :items="trans" :fields="transactionFields" stacked="sm" head-row-variant="secondary" striped>
+      <b-table :items="currentBeneficiaryTransactions" :fields="transactionFields" stacked="sm" head-row-variant="secondary" striped>
         <template v-slot:cell(amount)="data">
           <span class="text-secondary font-weight-bold"> {{ data.item.amount }}</span>
         </template>
@@ -121,31 +126,6 @@ export default {
         },
         'amount',
         'status'
-      ],
-      items: [
-        {
-          name: '', createdAt: '2020-07-06T11:23:29.068+00:00', addedBy: '605ffc7344792a551722d5903ba7463a', progress: 0
-        },
-        {
-          name: 'Mama Yake 2', createdAt: '2020-07-06T11:23:29.068+00:00', addedBy: '605ffc7344792a551722d5903ba7463a', progress: 50
-        },
-        {
-          name: 'Mama Yake 3', createdAt: '2020-07-06T11:23:29.068+00:00', addedBy: 'Middleman John Doe', progress: 75
-        },
-        {
-          name: 'Mama Yake 4', createdAt: '2020-07-06T11:23:29.068+00:00', addedBy: 'Middleman John Doe', progress: 100
-        }
-      ],
-      trans: [
-        {
-          updatedAt: '2020-07-06T11:23:29.068+00:00', addedBy: '605ffc7344792a551722d5903ba7463a', amount: 2000, status: 'pending'
-        },
-        {
-          updatedAt: '2020-06-06T11:23:29.068+00:00', addedBy: '605ffc7344792a551722d5903ba7463a', amount: 1000, status: 'success'
-        },
-        {
-          updatedAt: '2020-06-06T11:23:29.068+00:00', addedBy: 'Middleman John Doe', amount: 2000, status: 'failed'
-        }
       ]
     }
   }, 
@@ -186,6 +166,12 @@ export default {
     },
     getDate(datetime) {
       return new Date(datetime).toLocaleDateString();
+    },
+    getProgress(id) {
+      const thirtyDays = 30*24*60*60*1000;
+      const monthTransactions = this.currentBeneficiaryTransactions.filter(t => new Date().getTime() - new Date(t.updatedAt).getTime() < thirtyDays);
+      const monthTotal =  monthTransactions.reduce((acc, t) => { acc + t.amount; }, 0);
+      return monthTotal*100/2000;
     }
   },
   async mounted() {
