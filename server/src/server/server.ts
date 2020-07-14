34cd0694cf77cfad6c4303598219baae21580986
Jason Mahirwe <bjthe2nd@gmail.com>
2020-07-14 21:57:00 +0200

    Set number of requests per minute to 1000

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -8,7 +8,7 @@ import * as rateLimit from 'express-rate-limit';
 export function createServer(app: App) {
   const server = express();
   server.use(helmet());
-  server.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 35 })); // Limit each IP to 35 requests per minute
+  server.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 1000 })); // Limit each IP to 1000 requests per minute
   server.use(express.json());
   server.use(cors());
   server.use(injectApp(app));
