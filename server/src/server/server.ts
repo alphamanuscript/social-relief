0be7896ceb689f1cc7e849d2362f9f0ad57b9833
Jason Mahirwe <bjthe2nd@gmail.com>
2020-07-14 21:21:41 +0200

    Increase number of requests per minute for each IP

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -8,7 +8,7 @@ import * as rateLimit from 'express-rate-limit';
 export function createServer(app: App) {
   const server = express();
   server.use(helmet());
-  server.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 20 })); // Limit each IP to 20 requests per minute
+  server.use(rateLimit({ windowMs: 1 * 60 * 1000, max: 35 })); // Limit each IP to 35 requests per minute
   server.use(express.json());
   server.use(cors());
   server.use(injectApp(app));
