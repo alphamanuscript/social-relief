b0c9c6d8f8f2c4bc886f9aa426601fdf47a43b4f
Jason Mahirwe <bjthe2nd@gmail.com>
2020-07-15 19:15:42 +0200

    Rename sidebar link 'History' to 'Transaction history'

--- webapp/src/views/history.vue
+++ webapp/src/views/history.vue
@@ -14,6 +14,7 @@
           <div  align="end">
             <span class="font-weight-bold">Total amount distributed: </span><span class="font-weight-bold text-secondary">Ksh {{ totalAmountDistributed }}</span>
           </div>
+          
         </b-col>
       </b-row>
       <b-row v-if="!transactionItems.length" class="text-center ">
