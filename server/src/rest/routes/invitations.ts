96b8dd843494ca497785449ca3d82cbcf04dc81b
Jason Mahirwe <bjthe2nd@gmail.com>
2020-07-23 16:02:03 +0200

    Do not require authentication for fetching/updating a specific invitation

--- server/src/rest/routes/invitations.ts
+++ server/src/rest/routes/invitations.ts
@@ -3,9 +3,7 @@ import { wrapResponse, requireAuth } from '../middleware';
 
 export const invitations = Router();
 
-invitations.use(requireAuth());
-
-invitations.get('/', wrapResponse(
+invitations.get('/', requireAuth(), wrapResponse(
   req => req.core.invitations.getAllByUser(req.user._id, req.user.phone)));
 
 invitations.get('/:id', wrapResponse(
