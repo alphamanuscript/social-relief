e546ba217a6a1deaeb16afa0d425212ffead2ae4
Jason Mahirwe <bjthe2nd@gmail.com>
2020-07-28 23:04:15 +0200

    Implement user-notification service

---
+++ server/src/core/user-notification/types.ts
@@ -0,0 +1,12 @@
+import { EventBus } from '../event';
+import { AtSMSProvider } from '../sms';
+
+export interface UserNotificationsArgs {
+  smsProvider: AtSMSProvider;
+  eventBus: EventBus;
+  webappBaseUrl: string;
+}
+
+export interface UserInvitationEvent {
+  
+}
