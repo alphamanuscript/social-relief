62c37367a45b131811454e98553a85351b8be161
Jason Mahirwe <bjthe2nd@gmail.com>
2020-08-06 21:06:45 +0200

    Add more field value pairs in pwa.iconPaths

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -3,7 +3,10 @@ module.exports = {
     name: 'Social Relief',
     iconPaths: {
       favicon32: 'favicon.svg',
-      favicon16: 'favicon.svg'
+      favicon16: 'favicon.svg',
+      appleTouchIcon: 'favicon.svg',
+      maskIcon: 'favicon.svg',
+      msTileImage: 'favicon.svg',
     },
     themeColor: '#EF5A24',
     msTileColor: '#EF5A24',
