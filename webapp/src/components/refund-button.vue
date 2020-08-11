b028d05b94911d6925d94d44772553784965a679
Clément Habinshuti <hello@habbes.xyz>
2020-08-12 01:45:04 +0300

    Show dialog to confirm refund request

--- webapp/src/components/refund-button.vue
+++ webapp/src/components/refund-button.vue
@@ -4,21 +4,53 @@
       v-if="!areTransactionsBlocked"
       class="mt-2 form-control"
       variant="danger"
-      @click="requestRefund"
+      @click="showDialog()"
     >Request refund</b-button>
 
     <div
       v-if="isRefundPending"
       class="font-italic mt-2"
       >Refund is pending...</div>
+    
+    <b-modal
+      id="refundDialog"
+      size="sm" 
+      centered
+      hide-header-close
+      header-class="border-bottom-0"
+      hide-footer
+      no-stacking
+      content-class="rounded"
+    >
+      <template v-slot:modal-header>
+        <ModalHeader title="Request Refund" />
+      </template>
+      
+      <p>
+        This will
+      transfer to your registered M-Pesa number
+      the balance that has not been distributed to beneficiaries.
+      </p>
+      <p>
+        <small>If you exceed 2 refunds, you will not longer be allowed to make donations
+        on the platform.</small>
+      </p>
+
+      <div class="mt-3 d-flex flex-row-reverse justify-content">
+        <b-button variant="primary" class="mx-2" @click.prevent="handleProceed()">Proceed</b-button>
+        <b-button variant="danger" class="mx-2" @click.prevent="hideDialog()">Cancel</b-button>
+      </div>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex';
+import ModalHeader from '../ui-components/modal-header';
 
 export default {
   name: 'refund-button',
+  components: { ModalHeader },
   computed: {
     ...mapState(['user']),
     areTransactionsBlocked() {
@@ -30,8 +62,15 @@ export default {
   },
   methods: {
     ...mapActions(['initiateRefund']),
-    requestRefund() {
+    handleProceed() {
       this.initiateRefund();
+      this.hideDialog();
+    },
+    showDialog() {
+      this.$bvModal.show('refundDialog');
+    },
+    hideDialog() {
+      this.$bvModal.hide('refundDialog');
     }
   }
 
