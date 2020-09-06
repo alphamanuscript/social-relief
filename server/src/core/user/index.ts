b89e6d7e4edfb7980c4e702eb6fa7b18b65a170e
Jason Mahirwe <bjthe2nd@gmail.com>
2020-09-06 17:28:25 +0200

    Implement method donateAnonymously in user service

--- server/src/core/user/index.ts
+++ server/src/core/user/index.ts
@@ -1,2 +1,2 @@
-export { User, UserCreateArgs, UserService, UserRole, UserInvitationEventData, AnonymousService } from './types';
+export { User, UserCreateArgs, UserService, UserRole, UserInvitationEventData } from './types';
 export { Users } from './user-service';
