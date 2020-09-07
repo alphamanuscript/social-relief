472b2fad5d85731389596c5874954415986892e9
Jason Mahirwe <bjthe2nd@gmail.com>
2020-09-07 21:28:40 +0200

    Do not require authentication for endpoint /donations/anonymous/initiate

--- server/src/rest/routes/anonymous.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { wrapResponse } from '../middleware';
-
-export const anonymous = Router();
-
-anonymous.post('/donate', wrapResponse(
-  req => req.core.users.donateAnonymously(req.body)));
-
