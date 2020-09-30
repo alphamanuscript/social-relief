4fe1fb958cc92d7d131522d99f0f81d2c0c0ef55
Jason Mahirwe <bjthe2nd@gmail.com>
2020-09-30 21:49:16 +0200

    Implement worker for distributing funds to vetted and verified beneficiaries

---
+++ server/src/worker/vetted-beneficiary-distribution-worker.ts
@@ -0,0 +1,14 @@
+import { App } from '../core';
+
+export function runVettedBeneficiaryDistributionWorker(app: App, intervalMilliseconds: number) {
+  async function workLoop() {
+    console.log(`Starting distribution process for vetted beneficiaries at ${new Date()}...`);
+    const result = await app.donationDistributions.distributeDonations();
+    console.log(`Completed distribution process for vetted beneficiaries at ${new Date()}`);
+    console.log(result);
+    console.log();
+    setTimeout(workLoop, intervalMilliseconds);
+  }
+
+  workLoop();
+}
