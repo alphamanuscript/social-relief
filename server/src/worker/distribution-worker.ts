81b61b260702179c304675116dc8839d36137d71
Jason Mahirwe <bjthe2nd@gmail.com>
2020-10-02 00:16:41 +0200

    Log errors but do not process-exit when running both distribution plans

--- server/src/worker/distribution-worker.ts
+++ server/src/worker/distribution-worker.ts
@@ -2,12 +2,17 @@ import { App } from '../core';
 
 export function runDistributionWorker(app: App, intervalMilliseconds: number) {
   async function workLoop() {
-    console.log(`Starting distribution process at ${new Date()}...`);
-    const result = await app.donationDistributions.distributeDonations();
-    console.log(`Completed distribution process at ${new Date()}`);
-    console.log(result);
-    console.log();
-    setTimeout(workLoop, intervalMilliseconds);
+    try { 
+      console.log(`Starting distribution process at ${new Date()}...`);
+      const result = await app.donationDistributions.distributeDonations();
+      console.log(`Completed distribution process at ${new Date()}`);
+      console.log(result);
+      console.log();
+      setTimeout(workLoop, intervalMilliseconds);
+    }
+    catch(e) {
+      console.error(e);
+    }
   }
 
   workLoop();
--- server/src/worker/vetted-beneficiary-distribution-worker.ts
+++ server/src/worker/vetted-beneficiary-distribution-worker.ts
@@ -2,12 +2,17 @@ import { App } from '../core';
 
 export function runVettedBeneficiaryDistributionWorker(app: App, intervalMilliseconds: number) {
   async function workLoop() {
-    console.log(`Starting distribution process for vetted beneficiaries at ${new Date()}...`);
-    const result = await app.donationDistributions.distributeDonations(true);
-    console.log(`Completed distribution process for vetted beneficiaries at ${new Date()}`);
-    console.log(result);
-    console.log();
-    setTimeout(workLoop, intervalMilliseconds);
+    try {
+      console.log(`Starting distribution process for vetted beneficiaries at ${new Date()}...`);
+      const result = await app.donationDistributions.distributeDonations(true);
+      console.log(`Completed distribution process for vetted beneficiaries at ${new Date()}`);
+      console.log(result);
+      console.log();
+      setTimeout(workLoop, intervalMilliseconds);
+    }
+    catch(e) {
+      console.error(e);
+    }
   }
 
   workLoop();
