6efa0e21a2bbf6a2d8da30fff06a5e897a83d821
Jason Mahirwe <bjthe2nd@gmail.com>
2021-01-11 15:06:44 +0200

    Remove event emitter emitUserActivated in User service method activateMiddleman

--- server/src/core/user/user-service.ts
+++ server/src/core/user/user-service.ts
@@ -333,7 +333,6 @@ export class Users implements UserService {
         },
         { upsert: true, returnOriginal: false, projection: NOMINATED_USER_PROJECTION }
       );
-      this.eventBus.emitUserActivated({ user: getSafeUser(result.value) });
       return getSafeUser(result.value);
     }
     catch (e) {
