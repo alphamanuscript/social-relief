ef351aea4501ec06985ad1cb0d85131aa367b6fd
Jason Mahirwe <bjthe2nd@gmail.com>
2021-01-12 16:06:59 +0200

    Create endpoint for requesting resend of phone verification code

--- server/src/rest/routes/verifications.ts
+++ server/src/rest/routes/verifications.ts
@@ -6,6 +6,9 @@ export const verifications = Router();
 verifications.put('/phone/:id', wrapResponse(
   req => req.core.phoneVerification.confirmVerificationCode(req.params.id, req.body.code)));
 
+verifications.put('/phone/resend/code/:id', wrapResponse(
+  req => req.core.phoneVerification.resendVerificationCode(req.params.id)));
+
 verifications.get('/phone/:id', wrapResponse(
   req => req.core.phoneVerification.getById(req.params.id)));
 
